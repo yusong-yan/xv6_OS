b3ba63ad53a4974d093885f59e42b5e2f01286c6
yusong yan <yusong.yan@shiphawk.com>
2021-03-27 13:23:56 -0700

    ..

--- kernel/defs.h
+++ kernel/defs.h
@@ -191,6 +191,9 @@ int             copyout(pagetable_t, uint64, char *, uint64);
 int             copyin(pagetable_t, char *, uint64, uint64);
 int             copyinstr(pagetable_t, char *, uint64, uint64);
 
+// vmcopyin.c
+int             copyin_new(pagetable_t, char*, uint64, uint64);
+
 // plic.c
 void            plicinit(void);
 void            plicinithart(void);
